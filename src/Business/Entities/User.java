4c7f7ff20cd69a0d2890201694d391c894ba448f
ByGery <gerard.casassas@students.salle.url.edu>
2024-04-03 11:52:19 +0200

    DataBase Configuration
    Configuracio de la Base de Dades
    DAO dels metodes de User
    DAO dels metodes de Generators
    DAO dels metodes de Game
    Creacio de Entity Generator
    Creacio de Entity User

---
+++ src/Business/Entities/User.java
@@ -0,0 +1,38 @@
+package Business.Entities;
+
+public class User {
+
+    private final int id;
+    private final String email;
+    private final String username;
+    private final String password;
+
+    /**
+     * constructor of the class User, it contains the credentials of the people who uses the program
+     *
+     * @param id       integer that contains the number that identifies the user in the database
+     * @param email    string that contains the mail address of the user
+     * @param username is a string that allows us to identify a user
+     * @param password used to prove who you are when connecting to database
+     */
+    public User(int id, String username, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    /**
+     * constructor of the class User, it contains the credentials of the people who uses the program
+     *
+     * @param username is a string that allows us to identify a user
+     * @param email    string that contains the mail address of the user
+     * @param password used to prove who you are when connecting to database
+     */
+    public User(String username, String email, String password) {
+        this.id = 0;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+}
---
+++ src/Persistance/sqlDAO/SQLUserDAO.java
@@ -0,0 +1,113 @@
+package Persistance.sqlDAO;
+
+import Business.Entities.User;
+import Persistance.Connector;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class SQLUserDAO{
+
+    /**
+     * Receives nothing and does nothing
+     */
+    public SQLUserDAO() {
+    }
+
+    /**
+     * function that adds a user into the database
+     * @return boolean that indicates if the user has been added correctly
+     */
+
+    /*
+    public boolean addUser(User user) {
+        String name = user.getFullName();
+        String query = "INSERT INTO users(UserName, Email, Password) VALUES ('" + name + "', '" + user.getEmail() + "', '" + user.getPassword() + "');";
+        return Connector.getInstance().insertQuery(query);
+    }
+    */
+
+
+    /**
+     * function that returns all the users in the database
+     * @return List of the type User that contains all the Users in our database
+     */
+    public List<User> getAllUsers() {
+        List<User> users = new LinkedList<>();
+        String query = "SELECT id, UserName, Email, Password FROM users;";
+        ResultSet result = Connector.getInstance().selectQuery(query);
+
+        try {
+            while(result.next()) {
+                int id = result.getInt("id");
+                String username = result.getString("UserName");
+                String email = result.getString("Email");
+                String password = result.getString("Password");
+                users.add(new User(id, username, email, password));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return users;
+    }
+
+    /**
+     * function that checks if the user entered while signing in already is registered
+     * @return boolean that indicates if it exists or not
+     */
+    public boolean userExist(String user) {
+        try {
+            String query = "SELECT id FROM users WHERE UserName = '" + user + "';";
+            ResultSet result = Connector.getInstance().selectQuery(query);
+            return result.next();
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+    /**
+     * function that checks if the email entered while signing in already is registered
+     * @return boolean that indicates if it exists or not
+     */
+    public boolean emailExist(String email) {
+        try {
+            String query = "SELECT id FROM users WHERE Email = '" + email + "';";
+            ResultSet result = Connector.getInstance().selectQuery(query);
+            return result.next();
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+    /**
+     * function that deletes a user from the database
+     * @return boolean that indicates if it has been deleted correctly
+     */
+    public boolean deleteUser (int id) {
+
+        boolean check;
+        String query = "DELETE FROM users WHERE id = '" + id + "';";
+        Connector.getInstance().deleteQuery(query);
+        return true;
+    }
+
+    /**
+     * function that checks if the password entered by the user is valid
+     * @param password is a string with the entered password by the user
+     * @return boolean that indicates if the password introduced by the user is valid (follows the format in the statement)
+     */
+    public boolean checkPassword (String password) {
+        try {
+            String query = "SELECT id FROM users WHERE Password = '" + password + "';";     //Revisar
+            ResultSet result = Connector.getInstance().selectQuery(query);
+            return result.next();
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+}
