1d4d143c1880b56fa8e151c2a101f8fd21e43e99
ByGery <gerard.casassas@students.salle.url.edu>
2024-04-17 08:05:15 +0200

    startGameBBDD

--- src/Persistance/sqlDAO/SQLGameDAO.java
+++ src/Persistance/sqlDAO/SQLGameDAO.java
@@ -4,6 +4,7 @@ import Persistance.Connector;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -131,6 +132,39 @@ public class SQLGameDAO {
     public void upadteUserGameData(int id){
         String query = "UPDATE FROM game WHERE id = "+id+";";
         Connector.getInstance().updateQuery(query);
+    }
+
+    public int getGameCount(int userId) {
+        int gameCount = 0;
+        String query = "SELECT * FROM game WHERE ID_P = " + userId + ";";
+        ResultSet result = Connector.getInstance().selectQuery(query);
+        try {
+            while(result.next()) {
+                gameCount++;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return gameCount;
+    }
+
+    public boolean startNewGame(int userId) {
+        // Establecer los valores iniciales para la nueva partida
+        int numGame = 0;
+        int N_Coffees = 0;
+        int PowerUpClicker = 0;
+        boolean Ended = false;
+        String Time = "00:00:00"; // Asume que el tiempo se almacena como una cadena en formato HH:MM:SS
+
+        //Encontrar el numero de partidas que lleva ejecutadas el usuario
+        numGame = getGameCount(userId);
+
+        // Crear la consulta SQL para insertar la nueva partida
+        String query = "INSERT INTO game(ID_P, ID_G, N_Coffees, PowerUpClicker, Time, Ended) VALUES " +
+                "("+userId+", "+numGame+", "+N_Coffees+", "+PowerUpClicker+", '"+Time+"', "+Ended+");";
+
+        // Ejecutar la consulta y devolver si se realizó con éxito
+        return Connector.getInstance().insertQuery(query);
     }
 
 }
