d0917e1f8b0430a829b10f629905fdb2acc83ae7
AlbertoMarquillas <alberto.marquillas@students.salle.url.edu>
2024-04-21 10:07:44 +0200

    Controlers i alguna vista
    He creat una classe StatsView perque falta la vista d'estadistiques. També he creat un MainView per tenir totes les vistes controlades i poder definir-les juntes, junt amb un ChangeViewController que s'encarrega de la logica per canviar de pantalla. A mes a mes he acabat els controllers principals.

--- src/Presentation/Controller/StartController.java
+++ src/Presentation/Controller/StartController.java
@@ -1,4 +1,26 @@
 package Presentation.Controller;
 
-public class StartController {
+import Presentation.View.StartView;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class StartController implements ActionListener {
+
+    private final ChangeViewController changeViewController;
+
+    public StartController(ChangeViewController changeViewController) {
+        this.changeViewController = changeViewController;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("NewGame")) {
+            changeViewController.changePan("game");
+        } else if (e.getActionCommand().equals("ResumeGame")) {
+            changeViewController.changePan("game");
+        } else if (e.getActionCommand().equals("stats")) {
+            changeViewController.changePan("stats");
+        }
+    }
 }
