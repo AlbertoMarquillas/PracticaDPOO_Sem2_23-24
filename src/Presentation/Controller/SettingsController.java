23f7b5c9fefead7e924e253f375597b098444611
AlbertoMarquillas <alberto.marquillas@students.salle.url.edu>
2024-04-21 10:30:17 +0200

    Main creat

--- src/Presentation/Controller/SettingsController.java
+++ src/Presentation/Controller/SettingsController.java
@@ -1,4 +1,11 @@
 package Presentation.Controller;
 
-public class SettingsController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SettingsController implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
