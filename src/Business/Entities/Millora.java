370a9b26705d3630146952d6e78f59d7a1aa5893
ByGery <gerard.casassas@students.salle.url.edu>
2024-05-14 19:13:11 +0200

    Canvis de la produccioGlobal per Actual

--- src/Business/Entities/Millora.java
+++ src/Business/Entities/Millora.java
@@ -5,7 +5,7 @@ public class Millora {
     private static final double INCREMENT_A = 100.0; // Incremento para el tipo A
     private static final double INCREMENT_B = 200.0; // Incremento para el tipo B
     private static final double INCREMENT_C = 300.0; // Incremento para el tipo C
-    private static final double INCREMENT_D = 400.0; // Incremento para el tipo D
+    private static final double INCREMENT_D = 15.0; // Incremento para el tipo D
 
     private String type;
     private double preu; //cost del valor de millora
